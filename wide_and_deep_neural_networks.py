80fafd3a156e6782eac510ab2f8cd34ddbfa622a
Andy Nguyen <anhoainguyen.md@gmail.com>
2022-03-15 11:22:35 +1100

    Add .gitignore file

--- wide_and_deep_neural_networks.py
+++ wide_and_deep_neural_networks.py
@@ -16,12 +16,37 @@ def arg_parse():
 
     return parser.parse_args()
 
+def predict_with_wdnn(input, features, labels, forest_preprocessing=True):
+    """
+        Traing with cross validation
+    """
+    df = pd.read_csv(input)
+    X = df[features]
+
+    for label in labels:
+        Y = df[label]
+
+        # Preprocess feature with tree-based model
+        if forest_preprocessing:
+            pass
+
+    
+
+
 def main():
     args = arg_parse()
+    config_path = args['config_path']
     
     # load config file
-    pass
-
+    with open(config_path, "r") as config_file:
+        config = json.load(config_file)
+    
+    input = config["input_path"]
+    features = config["feature_col_index"]
+    labels = config["label_cols"]
+    forest_preprocessing = config["forest_preprocessing"] == 1
+    
+    predict_with_wdnn(input, features, labels, forest_preprocessing)
 
 if __name__ == "__main__":
     main()
